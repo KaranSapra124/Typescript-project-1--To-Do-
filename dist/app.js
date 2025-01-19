30cf918e9d09945de1385225a7547f7d65a07378
KaranSapra124 <sapra7112@gmail.com>
2025-01-20 01:29:36 +0530

    Finished Styling

--- dist/app.js
+++ dist/app.js
@@ -26,16 +26,16 @@ function addTask(item) {
 // Read Task
 function displayTasks(item) {
     taskDiv.innerHTML = "";
-    item === null || item === void 0 ? void 0 : item.map((elem, index) => {
-        if (taskDiv) {
-            return (taskDiv.innerHTML += `<div class="flex py-1  my-2 rounded-l-full rounded-r-full px-4 items-center  gap-4 text-white bg-gray-800 w-96 justify-between "><div><h1 class=${(elem === null || elem === void 0 ? void 0 : elem.isCompleted)
-                ? "line-through"
-                : "text-xl font-semibold"}>${elem.title} </h1></div>
+    (item === null || item === void 0 ? void 0 : item.length) > 0
+        ? item === null || item === void 0 ? void 0 : item.map((elem, index) => {
+            if (taskDiv) {
+                return (taskDiv.innerHTML += `<div class="flex py-1 h-fit mx-2 my-2 rounded-l-full rounded-r-full px-4 items-center  gap-4 text-white bg-gray-800 w-96 justify-between "><div><h1 class=${(elem === null || elem === void 0 ? void 0 : elem.isCompleted) ? "line-through" : "text-xl font-semibold"}>${elem.title} </h1></div>
     <div class="flex items-center"> <input  ${(elem === null || elem === void 0 ? void 0 : elem.isCompleted) ? "checked" : ""}
      } type="checkbox" class="h-5 my-auto w-5" onclick="isChecked(${index})" /> 
       <button class="bg-gray-500/70 font-semibold mx-2 text-white rounded-lg p-1 my-1" onclick="deleteTask(${index})">Delete</button></div></div>`); // Using += to append to existing content
-        }
-    });
+            }
+        })
+        : (taskDiv.innerHTML = `<h1 class="text-center text-2xl text-center mx-auto text-white font-extrabold">No Data Present</h1>`);
 }
 // IsCompleted
 function isChecked(index) {
@@ -57,4 +57,7 @@ function deleteTask(item) {
 }
 btn === null || btn === void 0 ? void 0 : btn.addEventListener("click", function () {
     addTask({ title: (taskInputElement === null || taskInputElement === void 0 ? void 0 : taskInputElement.value) || "", isCompleted: false });
+});
+window.addEventListener("load", function () {
+    displayTasks(taskArr);
 });
--- src/app.ts
+++ src/app.ts
@@ -31,6 +31,7 @@ setInterval(() => {
   } s`;
   dateTime.innerHTML = time;
 }, 1000);
+
 // Add Task
 function addTask(item: Task) {
   taskArr.push(item);
@@ -41,20 +42,20 @@ function addTask(item: Task) {
 // Read Task
 function displayTasks(item: Task[]) {
   taskDiv.innerHTML = "";
-  item?.map((elem: Task, index: number) => {
-    if (taskDiv) {
-      return (taskDiv.innerHTML += `<div class="flex py-1  my-2 rounded-l-full rounded-r-full px-4 items-center  gap-4 text-white bg-gray-800 w-96 justify-between "><div><h1 class=${
-        elem?.isCompleted
-          ? "line-through"
-          : "text-xl font-semibold"
-      }>${elem.title} </h1></div>
+  item?.length > 0
+    ? item?.map((elem: Task, index: number) => {
+        if (taskDiv) {
+          return (taskDiv.innerHTML += `<div class="flex py-1 h-fit mx-2 my-2 rounded-l-full rounded-r-full px-4 items-center  gap-4 text-white bg-gray-800 w-96 justify-between "><div><h1 class=${
+            elem?.isCompleted ? "line-through" : "text-xl font-semibold"
+          }>${elem.title} </h1></div>
     <div class="flex items-center"> <input  ${
       elem?.isCompleted ? "checked" : ""
     }
      } type="checkbox" class="h-5 my-auto w-5" onclick="isChecked(${index})" /> 
       <button class="bg-gray-500/70 font-semibold mx-2 text-white rounded-lg p-1 my-1" onclick="deleteTask(${index})">Delete</button></div></div>`); // Using += to append to existing content
-    }
-  });
+        }
+      })
+    : (taskDiv.innerHTML = `<h1 class="text-center text-2xl text-center mx-auto text-white font-extrabold">No Data Present</h1>`);
 }
 
 // IsCompleted
@@ -81,4 +82,8 @@ function deleteTask(item: number) {
 
 btn?.addEventListener("click", function () {
   addTask({ title: taskInputElement?.value || "", isCompleted: false });
+});
+
+window.addEventListener("load", function () {
+  displayTasks(taskArr);
 });
